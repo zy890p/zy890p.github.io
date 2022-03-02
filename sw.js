f6c825ad5008325012ae8cdc0ff226a41a28b38f
zy890p <zy890p@gmail.com>
2022-03-02 15:58:14 +0800

    updated

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
 var cacheName = 'hello-pwa';
 
+/*
 var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/main.js'
 ];
+*/
+
+var filesToCache = [
+  '/',
+  '/css/style.css',
+  '/js/main.js'
+];
+
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
