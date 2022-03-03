d00fa8417acc3a3d3ff57d72e02ca067bf1abce8
zy890p <zy890p@gmail.com>
2022-03-03 11:59:36 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 window.onload = () => {
   'use strict';
 
+  /*
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
   }
+  */
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('.//sw.js', {scope: 'sw-test'}).then(function(registration) {
+      // registration worked
+      console.log('Registration succeeded.');
+      //button.onclick = function() {
+        registration.update();
+      //}
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  };
+  
 }
